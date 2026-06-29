2a56ddc0970f9cba3dea23de4b644c0ded2c9a61
fibraraq <fibraraq@gmail.com>
2026-06-29 08:32:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Tokio Sushi - Núcleo de Operaciones y Control del Sistema
 
-const API_OBTENER_PEDIDOS = "http://localhost:5678/webhook/obtener-pedidos";
-const API_ACTUALIZAR_ESTADO = "http://localhost:5678/webhook/actualizar-estado";
-const URL_NUEVO_PEDIDO = "http://localhost:5678/webhook/Prueba-tokyo";
-const URL_OBTENER_MENU = "http://localhost:5678/webhook/obtener-menu";
-const URL_OBTENER_USUARIOS = "http://localhost:5678/webhook/obtener-usuarios";
+const API_OBTENER_PEDIDOS = "https://n8n-production-633e.up.railway.app/webhook/obtener-pedidos";
+const API_ACTUALIZAR_ESTADO = "https://n8n-production-633e.up.railway.app/webhook/actualizar-estado";
+const URL_NUEVO_PEDIDO = "https://n8n-production-633e.up.railway.app/webhook/Prueba-tokyo";
+const URL_OBTENER_MENU = "https://n8n-production-633e.up.railway.app/webhook/obtener-menu";
+const URL_OBTENER_USUARIOS = "https://n8n-production-633e.up.railway.app/webhook/obtener-usuarios";
 
 // NUEVAS URLs PARA EL PANEL DE ADMINISTRACIÓN
-const URL_GUARDAR_PRODUCTO = "http://localhost:5678/webhook/guardar-producto";
-const URL_GUARDAR_COMBO = "http://localhost:5678/webhook/guardar-combo";
+const URL_GUARDAR_PRODUCTO = "https://n8n-production-633e.up.railway.app/webhook/guardar-producto";
+const URL_GUARDAR_COMBO = "https://n8n-production-633e.up.railway.app/webhook/guardar-combo";
 
 let USUARIOS_SISTEMA = [];
 let CATALOGO_PRODUCTOS = []; // Catálogo unificado para el buscador de pedidos
@@ -832,11 +832,11 @@ function cerrarModal() { document.getElementById('modalDetalle').classList.add('
 // --- LÓGICA EXCLUSIVA DEL PANEL DE ADMINISTRACIÓN (admin.html) ---
 // =====================================================================
 
-const ADMIN_URL_MENU = "http://localhost:5678/webhook/obtener-menu";
-const ADMIN_URL_GUARDAR_CAT = "http://localhost:5678/webhook/guardar-categoria";
-const ADMIN_URL_GUARDAR_PROD = "http://localhost:5678/webhook/guardar-producto";
-const ADMIN_URL_GUARDAR_COMBO = "http://localhost:5678/webhook/guardar-combo";
-const ADMIN_URL_ELIMINAR = "http://localhost:5678/webhook/eliminar-item";
+const ADMIN_URL_MENU = "https://n8n-production-633e.up.railway.app/webhook/obtener-menu";
+const ADMIN_URL_GUARDAR_CAT = "https://n8n-production-633e.up.railway.app/webhook/guardar-categoria";
+const ADMIN_URL_GUARDAR_PROD = "https://n8n-production-633e.up.railway.app/webhook/guardar-producto";
+const ADMIN_URL_GUARDAR_COMBO = "https://n8n-production-633e.up.railway.app/webhook/guardar-combo";
+const ADMIN_URL_ELIMINAR = "https://n8n-production-633e.up.railway.app/webhook/eliminar-item";
 
 let adminCategorias = [];
 let adminProductos = [];
